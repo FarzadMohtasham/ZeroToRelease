833ddf7fa30687eea085e0d69ae898ad3b541af6
Farzad Mohtasham <farzaddddmohtasham@gmail.com>
2024-03-16 15:24:16 +0330

    SASS/SCSS added to project

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,16 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 import eslint from 'vite-plugin-eslint';
+import sass from 'sass'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), eslint()],
+    plugins: [react(), eslint()],
+    css: {
+        preprocessorOptions: {
+            scss: {
+                implementation: sass,
+            }
+        }
+    }
 })
